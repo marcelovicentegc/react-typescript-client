dc9c7b7024a5c45e37287e82d7398a12d75d24be
Marcelo Cardoso <marcelovicentegc@gmail.com>
2020-01-02 02:56:31 -0300

    feature (button): onClick prop is called + renders secondary style

--- src/components/Button/__tests__/index.tsx
+++ src/components/Button/__tests__/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { Button } from "..";
+import { Button, ButtonType } from "..";
 import { render } from "../../../utils/render";
+import { fireEvent } from "@testing-library/react";
+import { theme } from "../../../utils/theme";
 
 describe("<Button /> test case", () => {
   test("test ids are in the document", () => {
@@ -10,5 +12,26 @@ describe("<Button /> test case", () => {
     expect(getByTestId("buttonWrapper")).toBeInTheDocument();
     expect(getByTestId("styledButton")).toBeInTheDocument();
     expect(getByTestId("label")).toBeInTheDocument();
+  });
+
+  test("onClick prop is called", () => {
+    const onClick = jest.fn();
+    const { getByTestId } = render(
+      <Button label="Click me" onClick={onClick} />
+    );
+
+    fireEvent.click(getByTestId("styledButton"), { button: 1 });
+    expect(onClick).toHaveBeenCalled();
+  });
+
+  test("renders secondary style", () => {
+    const { getByTestId } = render(
+      <Button label="Click me" type={ButtonType.secondary} />
+    );
+
+    expect(getByTestId("styledButton")).toHaveStyle(
+      `background-color: ${theme.color.white1};
+      color: ${theme.color.blue1};`
+    );
   });
 });

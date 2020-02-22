3c198f01867b6a4cafe833368394292b80a6a076
Marcelo Cardoso <marcelovicentegc@gmail.com>
2020-02-22 12:12:29 -0300

    fix: remove unused import

--- src/utils/render.tsx
+++ src/utils/render.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import { render as testingLibraryRender } from "@testing-library/react";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
-import {
-  LandingPageProvider,
-  LandingPageContextInterface
-} from "../contexts/LandingPageContext";
+import { LandingPageProvider } from "../contexts/LandingPageContext";
 
 export const render = (ui: React.ReactElement) => {
   return {
